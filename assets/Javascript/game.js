86594f055fd7610014ba14b77cbaecd5d3f4ab92
Macaroni Biff <corey.waitforit.mitchell@gmail.com>
2018-06-16 16:47:47 -0500

    finishedgit add .! now for the harder homework.

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -12,10 +12,12 @@ var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.l
 document.onkeyup = function(event) {
     //setting userGuess as keyboard input
     var userGuess = event.key;
+    
 
         //limiting user input to alphabetic characters
-        if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") ||(userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z") ){
-
+        if (computerChoices.indexOf(userGuess) !== -1){
+            
+        
             //setting up win scenerio. If you win; wins go up by one, guesses and guesses remaing reset, and the computer guesses a new letter.
             if (userGuess === computerGuess) {
                 wins++;
@@ -24,17 +26,18 @@ document.onkeyup = function(event) {
                 computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
             }
 
-            // else if (userGuess === guesses []){
-            //     do nothing
-            // }
+            //limits the user from being able to guess the same thing multiple times
+            else if (guesses.indexOf(userGuess) !== -1){
+               return;
+            } 
 
             //adds user guesses to the guesses array and subtracts from remaining guesses
             else {
-                guesses.push(userGuess)
+                guesses.push(userGuess);
                 lives--;
-            }
+            };
 
-            //setting up lose scenerio. When lives equal zero; guesses are reset, losses go up by one, remaining guesses resets and computer guesses a new letter.
+            //setting up loss scenerio. When lives equal zero; guesses are reset, losses go up by one, remaining guesses resets and the computer guesses a new letter.
             if (lives === 0){
                 guesses.length = 0;
                 losses++;
@@ -51,14 +54,8 @@ document.onkeyup = function(event) {
     "<h2>Wins: " + wins + "</h2>" +
     "<h2>Losses: " + losses + "</h2>" +
     "<h2>Guesses Left: " + lives + "</h2>" +
-    "<h2>Guesses So Far: " + guesses + "</h2>" ;
+    "<h2>Your Guesses So Far: " + guesses + "</h2>" ;
 
     document.querySelector("#starting-text").innerHTML = html;
 
 };
-
-
-
-
-//questions to ask: can i limit user input down to a character set without having to do if {userGuess === "a" || etc for every letter of the alphabet}.
-//is there a way to compare user input to items in an array?
